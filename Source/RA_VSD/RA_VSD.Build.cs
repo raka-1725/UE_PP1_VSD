c78c110bbb36f2f72916b97895753a0888a40537
Akaike <akaike@student.uiwtx.edu>
2026-08-28 13:05:28 -0500

    Vehicle Plugin installed

--- Source/RA_VSD/RA_VSD.Build.cs
+++ Source/RA_VSD/RA_VSD.Build.cs
@@ -8,7 +8,7 @@ public class RA_VSD : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ChaosVehicles" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
