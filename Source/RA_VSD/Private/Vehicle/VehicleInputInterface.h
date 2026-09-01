11c32f4275b5f8cbc6c08de94aa0ec1680dfea0f
raka_07 <raka3dprogramming@gmail.com>
2026-09-01 08:55:01 -0500

    VehicleInputInterface

---
+++ Source/RA_VSD/Private/Vehicle/VehicleInputInterface.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "VehicleInputInterface.generated.h"
+
+/**
+ * 
+ */
+//Minimal API for less exposed method
+UINTERFACE(MinimalAPI)
+class UVehicleInputInterface : public UInterface
+{
+	GENERATED_BODY()
+	
+};
+
+class IVehicleInputInterface
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void ApplySteer(float val) = 0;
+	virtual void ApplyThrottle(float val) = 0;
+	virtual void ApplyBrake(float val) = 0;
+	
+	virtual void OnPlayerControl() = 0;
+	virtual void OnAIControl() = 0;
+	virtual void OnControlReleased() = 0;
+	
+	virtual bool IsPlayerDriving() const = 0;
+};
