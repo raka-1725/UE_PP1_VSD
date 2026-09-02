6ae3ba07af7a616a484b856e5cb52cfcb95fb5b1
raka_07 <raka3dprogramming@gmail.com>
2026-09-02 08:39:32 -0500

    Added func for enter/exit

--- Source/RA_VSD/Private/Vehicle/VehicleInputInterface.h
+++ Source/RA_VSD/Private/Vehicle/VehicleInputInterface.h
@@ -31,4 +31,11 @@ public:
 	virtual void OnControlReleased() = 0;
 	
 	virtual bool IsPlayerDriving() const = 0;
+	
+	//Enter / Exit
+	/*
+	virtual void EnterVehicle(AController* NewDriver) = 0;
+	virtual void ExitVehicle(AController* Exit) = 0;
+	virtual bool CanEnterVehicle() const = 0;
+	*/
 };
