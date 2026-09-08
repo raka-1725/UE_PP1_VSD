e572ed7bdd361a413bdcd2778d00ca6a2fb1d2c1
raka_07 <raka3dprogramming@gmail.com>
2026-09-08 16:07:39 -0500

    Wheel component added, control still in progress

---
+++ Source/RA_VSD/Private/Vehicle/CChaosVehicleWheel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ChaosVehicleWheel.h"
+#include "CChaosVehicleWheel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCChaosVehicleWheel : public UChaosVehicleWheel
+{
+	GENERATED_BODY()
+	
+};
